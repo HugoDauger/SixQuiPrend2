c65456e2e3d8927e76f180c42000c4e6276be612
Clémentine DEBEUGNY <82205431+Clem1407@users.noreply.github.com>
2023-05-26 16:53:53 +0200

    Update HelloApplication.java

--- src/main/java/com/example/sixquiprend2/HelloApplication.java
+++ src/main/java/com/example/sixquiprend2/HelloApplication.java
@@ -1,20 +1,33 @@
-package com.example.sixquiprend2;
-
+package com.example.jeu6quiprend;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Jeu 6 Qui Prend");
+
+        // Création de la fenêtre de jeu
+        FenetreJeu fenetreJeu = new FenetreJeu();
+
+        // Création de la scène principale
+        Scene scene = new Scene(fenetreJeu, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        // Création et initialisation de la liste de cartes
+        List<Carte> cartes = new ArrayList<>();
+        cartes.add(new Carte(1));
+        cartes.add(new Carte(2));
+
+        // Dessiner le contenu du jeu
+        fenetreJeu.dessinerJeu();
+        fenetreJeu.dessinerPlateau();
+        fenetreJeu.dessinerCartes(cartes);
     }
 
     public static void main(String[] args) {

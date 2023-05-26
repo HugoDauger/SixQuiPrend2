00ca742203642f43d2e7e933430abd1dffc72a61
Clémentine DEBEUGNY <82205431+Clem1407@users.noreply.github.com>
2023-05-26 16:55:08 +0200

    Update Joueur.java

--- src/main/java/com/example/sixquiprend2/Joueur.java
+++ src/main/java/com/example/sixquiprend2/Joueur.java
@@ -1,8 +1,8 @@
-package com.example.sixquiprend2;
 package com.example.jeu6quiprend;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,6 +14,8 @@ public class Joueur {
     private String nom;
     private int score;
     private List<Carte> cartesEnMain;
+    private Carte carteDeposee;
+    private List<Carte> pileTetesDeBoeuf;
 
     public Joueur(String nom) {
         this.nom = nom;
@@ -22,20 +24,28 @@ public class Joueur {
     }
 
     public void ajouterCarteEnMain(Carte carte) {
+
         cartesEnMain.add(carte);
     }
 
     public void retirerCarteEnMain(Carte carte) {
+
         cartesEnMain.remove(carte);
     }
 
     public List<Carte> getCartesEnMain() {
+
         return cartesEnMain;
     }
 
     public void augmenterScore(int points) {
         score += points;
     }
+
+    public void ajouterCartePileTetesDeBoeuf(Carte carte) {
+        pileTetesDeBoeuf.add(carte);
+    }
+
 
     public void distribuerCartes(List<Carte> cartes) {
         int nbCartesDistribuees = Math.min(cartes.size(), 10); // Le nombre de cartes à distribuer (maximum de 10)
@@ -45,5 +55,39 @@ public class Joueur {
             ajouterCarteEnMain(carte);
         }
     }
-}
 
+    public Carte getCarteAleatoireEnMain() {
+        Random random = new Random();
+        int index = random.nextInt(cartesEnMain.size());
+        return cartesEnMain.get(index);
+    }
+
+    public void encaisserCartes(List<Carte> cartes) {
+        cartesEnMain.addAll(cartes);
+    }
+
+    public int choisirRangeeARemplacer(List<List<Carte>> rangees) {
+        // Afficher les rangées disponibles et permettre au joueur de choisir
+        System.out.println("Rangées disponibles : ");
+        for (int i = 0; i < rangees.size(); i++) {
+            List<Carte> rangée = rangees.get(i);
+            if (rangée.isEmpty()) {
+                System.out.println("Rangée " + (i + 1) + " : Vide");
+            } else {
+                System.out.println("Rangée " + (i + 1) + " : " + rangée.get(rangée.size() - 1).getNumero());
+            }
+        }
+
+        // Demander au joueur de choisir une rangée
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Choisissez le numéro de la rangée que vous voulez remplacer : ");
+        int choix = scanner.nextInt();
+
+        // Vérifier si le choix est valide
+        if (choix >= 1 && choix <= rangees.size()) {
+            return choix - 1;
+        } else {
+            return -1;
+        }
+    }
+}
